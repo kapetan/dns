7c32e58c8738c6ad155d546a6b37544033c5946b
a_mastrakov <a_mastrakov@skbkontur.ru>
2016-10-15 21:32:50 +0700

    added support of .net4.5 with async methods

--- DNS/Client/RequestResolver/IRequestResolver.cs
+++ DNS/Client/RequestResolver/IRequestResolver.cs
@@ -1,5 +1,8 @@
 ﻿namespace DNS.Client.RequestResolver {
     public interface IRequestResolver {
         ClientResponse Request(ClientRequest request);
+#if NET45
+        Task<ClientResponse> RequestAsync(ClientRequest request);
+#endif
     }
 }
--- DNS/Client/RequestResolver/TcpRequestResolver.cs
+++ DNS/Client/RequestResolver/TcpRequestResolver.cs
@@ -1,21 +1,27 @@
 ﻿using System;
 using System.IO;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using DNS.Protocol;
 
-namespace DNS.Client.RequestResolver {
-    public class TcpRequestResolver : IRequestResolver {
-        public ClientResponse Request(ClientRequest request) {
+namespace DNS.Client.RequestResolver
+{
+    public class TcpRequestResolver : IRequestResolver
+    {
+        public ClientResponse Request(ClientRequest request)
+        {
             TcpClient tcp = new TcpClient();
 
-            try {
+            try
+            {
                 tcp.Connect(request.Dns);
 
                 Stream stream = tcp.GetStream();
                 byte[] buffer = request.ToArray();
-                byte[] length = BitConverter.GetBytes((ushort) buffer.Length);
+                byte[] length = BitConverter.GetBytes((ushort)buffer.Length);
 
-                if (BitConverter.IsLittleEndian) {
+                if (BitConverter.IsLittleEndian)
+                {
                     Array.Reverse(length);
                 }
 
@@ -25,7 +31,8 @@ namespace DNS.Client.RequestResolver {
                 buffer = new byte[2];
                 Read(stream, buffer);
 
-                if (BitConverter.IsLittleEndian) {
+                if (BitConverter.IsLittleEndian)
+                {
                     Array.Reverse(buffer);
                 }
 
@@ -35,24 +42,85 @@ namespace DNS.Client.RequestResolver {
                 Response response = Response.FromArray(buffer);
 
                 return new ClientResponse(request, response, buffer);
-            } finally {
+            }
+            finally
+            {
                 tcp.Close();
             }
         }
 
-        private static void Read(Stream stream, byte[] buffer) {
+#if NET45
+        public async Task<ClientResponse> RequestAsync(ClientRequest request)
+        {
+            using (var tcp = new TcpClient())
+            {
+                await tcp.ConnectAsync(request.Dns.Address, request.Dns.Port);
+
+                Stream stream = tcp.GetStream();
+                byte[] buffer = request.ToArray();
+                byte[] length = BitConverter.GetBytes((ushort)buffer.Length);
+
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(length);
+                }
+
+                await stream.WriteAsync(length, 0, length.Length);
+                await stream.WriteAsync(buffer, 0, buffer.Length);
+
+                buffer = new byte[2];
+                Read(stream, buffer);
+
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(buffer);
+                }
+
+                buffer = new byte[BitConverter.ToUInt16(buffer, 0)];
+                await ReadAsync(stream, buffer);
+
+                Response response = Response.FromArray(buffer);
+
+                return new ClientResponse(request, response, buffer);
+            }
+        }
+#endif
+        private static void Read(Stream stream, byte[] buffer)
+        {
             int length = buffer.Length;
             int offset = 0;
             int size = 0;
 
-            while (length > 0 && (size = stream.Read(buffer, offset, length)) > 0) {
+            while (length > 0 && (size = stream.Read(buffer, offset, length)) > 0)
+            {
                 offset += size;
                 length -= size;
             }
 
-            if (length > 0) {
+            if (length > 0)
+            {
                 throw new IOException("Unexpected end of stream");
             }
         }
+
+#if NET45
+        private static async Task ReadAsync(Stream stream, byte[] buffer)
+        {
+            int length = buffer.Length;
+            int offset = 0;
+            int size = 0;
+
+            while (length > 0 && (size = await stream.ReadAsync(buffer, offset, length)) > 0)
+            {
+                offset += size;
+                length -= size;
+            }
+
+            if (length > 0)
+            {
+                throw new IOException("Unexpected end of stream");
+            }
+        }
+#endif
     }
 }

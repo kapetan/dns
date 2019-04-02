538746827bb6e8b77330fcac83c601f1611c9935
Emile Badenhorst <MyvarHD@gmail.com>
2019-04-02 18:06:57 +0200

    Update MasterFile.cs

--- DNS/Server/MasterFile.cs
+++ DNS/Server/MasterFile.cs
@@ -8,19 +8,15 @@ using DNS.Protocol;
 using DNS.Protocol.ResourceRecords;
 using DNS.Client.RequestResolver;
 
-namespace DNS.Server
-{
-    public class MasterFile : IRequestResolver
-    {
+namespace DNS.Server {
+    public class MasterFile : IRequestResolver {
         private static readonly TimeSpan DEFAULT_TTL = new TimeSpan(0);
 
-        private static bool Matches(Domain domain, Domain entry)
-        {
+        private static bool Matches(Domain domain, Domain entry) {
             string[] labels = entry.ToString().Split('.');
             string[] patterns = new string[labels.Length];
 
-            for (int i = 0; i < labels.Length; i++)
-            {
+            for (int i = 0; i < labels.Length; i++) {
                 string label = labels[i];
                 patterns[i] = label == "*" ? "(\\w+)" : Regex.Escape(label);
             }
@@ -29,10 +25,8 @@ namespace DNS.Server
             return re.IsMatch(domain.ToString());
         }
 
-        private static void Merge<T>(IList<T> l1, IList<T> l2)
-        {
-            foreach (T obj in l2)
-            {
+        private static void Merge<T>(IList<T> l1, IList<T> l2) {
+            foreach (T obj in l2) {
                 l1.Add(obj);
             }
         }
@@ -40,95 +34,75 @@ namespace DNS.Server
         private IList<IResourceRecord> entries = new List<IResourceRecord>();
         private TimeSpan ttl = DEFAULT_TTL;
 
-        public MasterFile(TimeSpan ttl)
-        {
+        public MasterFile(TimeSpan ttl) {
             this.ttl = ttl;
         }
 
-        public MasterFile()
-        {
-        }
+        public MasterFile() {}
 
-        public void Add(IResourceRecord entry)
-        {
+        public void Add(IResourceRecord entry) {
             entries.Add(entry);
         }
 
-        public void AddIPAddressResourceRecord(string domain, string ip)
-        {
-            AddIPAddressResourceRecord(new Domain(domain), IPAddress.Parse(ip));
-        }
-
-        public void AddSrvRecord(string service, string target, int port, int priority, int weight)
+		public void AddSrvRecord(string service, string target, int port, int priority, int weight)
         {
             Add(new SrvResourceRecord(Domain.FromString(service), priority, weight, port, Domain.FromString(target),
                 ttl));
         }
 
-        public void AddIPAddressResourceRecord(Domain domain, IPAddress ip)
-        {
+        public void AddIPAddressResourceRecord(string domain, string ip) {
+            AddIPAddressResourceRecord(new Domain(domain), IPAddress.Parse(ip));
+        }
+
+        public void AddIPAddressResourceRecord(Domain domain, IPAddress ip) {
             Add(new IPAddressResourceRecord(domain, ip, ttl));
         }
 
-        public void AddNameServerResourceRecord(string domain, string nsDomain)
-        {
+        public void AddNameServerResourceRecord(string domain, string nsDomain) {
             AddNameServerResourceRecord(new Domain(domain), new Domain(nsDomain));
         }
 
-        public void AddNameServerResourceRecord(Domain domain, Domain nsDomain)
-        {
+        public void AddNameServerResourceRecord(Domain domain, Domain nsDomain) {
             Add(new NameServerResourceRecord(domain, nsDomain, ttl));
         }
 
-        public void AddCanonicalNameResourceRecord(string domain, string cname)
-        {
+        public void AddCanonicalNameResourceRecord(string domain, string cname) {
             AddCanonicalNameResourceRecord(new Domain(domain), new Domain(cname));
         }
 
-        public void AddCanonicalNameResourceRecord(Domain domain, Domain cname)
-        {
+        public void AddCanonicalNameResourceRecord(Domain domain, Domain cname) {
             Add(new CanonicalNameResourceRecord(domain, cname, ttl));
         }
 
-        public void AddPointerResourceRecord(string ip, string pointer)
-        {
+        public void AddPointerResourceRecord(string ip, string pointer) {
             AddPointerResourceRecord(IPAddress.Parse(ip), new Domain(pointer));
         }
 
-        public void AddPointerResourceRecord(IPAddress ip, Domain pointer)
-        {
+        public void AddPointerResourceRecord(IPAddress ip, Domain pointer) {
             Add(new PointerResourceRecord(ip, pointer, ttl));
         }
 
-        public void AddMailExchangeResourceRecord(string domain, int preference, string exchange)
-        {
+        public void AddMailExchangeResourceRecord(string domain, int preference, string exchange) {
             AddMailExchangeResourceRecord(new Domain(domain), preference, new Domain(exchange));
         }
 
-        public void AddMailExchangeResourceRecord(Domain domain, int preference, Domain exchange)
-        {
+        public void AddMailExchangeResourceRecord(Domain domain, int preference, Domain exchange) {
             Add(new MailExchangeResourceRecord(domain, preference, exchange));
         }
 
-        public void AddTextResourceRecord(string domain, string attributeName, string attributeValue)
-        {
+        public void AddTextResourceRecord(string domain, string attributeName, string attributeValue) {
             Add(new TextResourceRecord(new Domain(domain), attributeName, attributeValue, ttl));
         }
 
-        public Task<IResponse> Resolve(IRequest request)
-        {
+        public Task<IResponse> Resolve(IRequest request) {
             IResponse response = Response.FromRequest(request);
 
-            foreach (Question question in request.Questions)
-            {
+            foreach (Question question in request.Questions) {
                 IList<IResourceRecord> answers = Get(question);
 
-                if (answers.Count > 0)
-                {
+                if (answers.Count > 0) {
                     Merge(response.AnswerRecords, answers);
-                }
-                else
-                {
+                } else {
                     response.ResponseCode = ResponseCode.NameError;
                 }
             }
@@ -136,13 +110,11 @@ namespace DNS.Server
             return Task.FromResult(response);
         }
 
-        private IList<IResourceRecord> Get(Domain domain, RecordType type)
-        {
+        private IList<IResourceRecord> Get(Domain domain, RecordType type) {
             return entries.Where(e => Matches(domain, e.Name) && e.Type == type).ToList();
         }
 
-        private IList<IResourceRecord> Get(Question question)
-        {
+        private IList<IResourceRecord> Get(Question question) {
             return Get(question.Name, question.Type);
         }
     }

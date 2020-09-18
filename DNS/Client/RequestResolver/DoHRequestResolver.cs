142313b1a514639a97330663db49b3e0b0060f39
Student Main <scncgz@gmail.com>
2020-09-18 23:07:37 +0800

    doh cloudflare json api support

--- DNS/Client/RequestResolver/DoHRequestResolver.cs
+++ DNS/Client/RequestResolver/DoHRequestResolver.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using DNS.Protocol;
+using DNS.Protocol.ResourceRecords;
+using Newtonsoft.Json;
 
 namespace DNS.Client.RequestResolver
 {
@@ -103,7 +107,35 @@ namespace DNS.Client.RequestResolver
                     }
                 case DoHProtocolMode.CloudflareJson:
                     {
-                        throw new NotImplementedException();
+                        if (request.Questions.Count != 1)
+                        {
+                            throw new NotSupportedException("DoH JSON API can only has 1 question per request");
+                        }
+                        Question q = request.Questions[0];
+
+                        UriBuilder ub = new UriBuilder(dns);
+                        System.Collections.Specialized.NameValueCollection query = HttpUtility.ParseQueryString(dns.Query);
+
+
+                        query["name"] = q.Name.ToString();
+                        query["type"] = q.Type.ToString();
+
+                        ub.Query = query.ToString();
+                        // GET /dns-query?dns={base64url(query)} HTTP/2
+                        // Accept: application/dns-message
+                        //
+                        HttpRequestMessage reqMsg = new HttpRequestMessage(HttpMethod.Get, ub.Uri);
+                        reqMsg.Headers.Clear();
+                        reqMsg.Headers.Add("Accept", "application/dns-json");
+                        HttpResponseMessage hrm = await http.SendAsync(reqMsg, cancellationToken);
+                        if (!hrm.IsSuccessStatusCode)
+                        {
+                            throw new WebException($"DoH server return code {hrm.StatusCode}");
+                        }
+                        string responseText = await hrm.Content.ReadAsStringAsync();
+                        DoHCloudflareJsonResponse responseJson = JsonConvert.DeserializeObject<DoHCloudflareJsonResponse>(responseText);
+                        response = responseJson.GetResponse();
+                        break;
                     }
                 default:
                     throw new NotSupportedException();
@@ -113,6 +145,150 @@ namespace DNS.Client.RequestResolver
             request.Id = id;
             response.Id = id;
             return new ClientResponse(request, response);
+        }
+    }
+
+    internal class DoHCloudflareJsonResponse
+    {
+        public int Status;
+        public bool TC;
+        public bool RD;
+        public bool RA;
+        public bool AD;
+        public bool CD;
+        public List<DoHCloudflareJsonResponseQuestion> Question;
+        public List<DoHCloudflareJsonResponseAnswer> Answer;
+        public List<DoHCloudflareJsonResponseAnswer> Authority;
+        public List<DoHCloudflareJsonResponseAnswer> Additional;
+
+        public Response GetResponse()
+        {
+            Response resp = new Response
+            {
+                Truncated = TC,
+                // RecursiveDesired = RD,
+                RecursionAvailable = RA,
+                AuthenticData = AD,
+                CheckingDisabled = CD,
+            };
+            foreach (DoHCloudflareJsonResponseQuestion q in Question)
+            {
+                resp.Questions.Add(q.GetQuestion());
+            }
+            foreach (DoHCloudflareJsonResponseAnswer a in Answer)
+            {
+                resp.AnswerRecords.Add(a.GetResourceRecord());
+            }
+            foreach (DoHCloudflareJsonResponseAnswer a in Authority)
+            {
+                resp.AuthorityRecords.Add(a.GetResourceRecord());
+            }
+            foreach (DoHCloudflareJsonResponseAnswer a in Additional)
+            {
+                resp.AdditionalRecords.Add(a.GetResourceRecord());
+            }
+            return resp;
+        }
+    }
+
+    internal class DoHCloudflareJsonResponseQuestion
+    {
+        public string name;
+        public int type;
+        public Question GetQuestion()
+        {
+            return new Question(Domain.FromString(name), (RecordType)Enum.ToObject(typeof(RecordType), type));
+        }
+    }
+
+    internal class DoHCloudflareJsonResponseAnswer
+    {
+        public string name;
+        public int type;
+        public int TTL;
+        public string data;
+        public IResourceRecord GetResourceRecord()
+        {
+            Domain domain = Domain.FromString(name);
+            TimeSpan ttl = TimeSpan.FromSeconds(TTL);
+            // not registered rr
+            if (!Enum.IsDefined(typeof(RecordType), type))
+            {
+                byte[] dataByte = Enumerable.Range(0, data.Length)
+                     .Where(x => x % 2 == 0)
+                     .Select(x => Convert.ToByte(data.Substring(x, 2), 16))
+                     .ToArray();
+                return new ResourceRecord(
+                    domain,
+                    dataByte,
+                    (RecordType)type,
+                    RecordClass.IN,
+                    ttl
+                    );
+            }
+            RecordType r = (RecordType)Enum.ToObject(typeof(RecordType), type);
+            // well known rr
+            switch (r)
+            {
+                case RecordType.CNAME:
+                    return new CanonicalNameResourceRecord(domain, Domain.FromString(data), ttl);
+                case RecordType.A:
+                case RecordType.AAAA:
+                    return new IPAddressResourceRecord(domain, IPAddress.Parse(data), ttl);
+                case RecordType.NS:
+                    return new NameServerResourceRecord(domain, Domain.FromString(data), ttl);
+                case RecordType.PTR:
+                    return new PointerResourceRecord(IPAddress.Parse(name), Domain.FromString(data), ttl);
+                case RecordType.MX:
+                    string[] mx = data.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (mx.Length != 2)
+                    {
+                        throw new FormatException();
+                    }
+
+                    return new MailExchangeResourceRecord(domain, int.Parse(mx[0]), Domain.FromString(mx[1]), ttl);
+                case RecordType.SRV:
+                    string[] srv = data.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (srv.Length != 4)
+                    {
+                        throw new FormatException();
+                    }
+
+                    return new ServiceResourceRecord(
+                        domain,
+                        ushort.Parse(srv[0]),
+                        ushort.Parse(srv[1]),
+                        ushort.Parse(srv[2]),
+                        Domain.FromString(srv[3]),
+                        ttl);
+                case RecordType.SOA:
+                    string[] soa = data.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (soa.Length != 7)
+                    {
+                        throw new FormatException();
+                    }
+
+                    return new StartOfAuthorityResourceRecord(
+                        domain,
+                        Domain.FromString(soa[0]),
+                        Domain.FromString(soa[1]),
+                        long.Parse(soa[2]),
+                        TimeSpan.FromSeconds(long.Parse(soa[3])),
+                        TimeSpan.FromSeconds(long.Parse(soa[4])),
+                        TimeSpan.FromSeconds(long.Parse(soa[5])),
+                        TimeSpan.FromSeconds(long.Parse(soa[6])),
+                        ttl);
+                case RecordType.TXT:
+                    return new TextResourceRecord(domain, CharacterString.FromString(data), ttl);
+            }
+            // registered but has no corresponding rr class
+            return new ResourceRecord(
+                    Domain.FromString(name),
+                    Encoding.ASCII.GetBytes(data),
+                    (RecordType)Enum.ToObject(typeof(RecordType), type),
+                    RecordClass.IN,
+                    TimeSpan.FromSeconds(TTL)
+                    );
         }
     }
 }

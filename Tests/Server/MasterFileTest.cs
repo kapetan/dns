db878ef2aa9b67d2d6b51e1a0a4525a960c95702
andreas.hoffmann <andreas.hoffmann@froebel-gruppe.de>
2021-07-28 21:13:29 +0200

    Refactored

--- Tests/Server/MasterFileTest.cs
+++ Tests/Server/MasterFileTest.cs
@@ -11,13 +11,18 @@ using Xunit;
 namespace DNS.Tests.Server {
 	public class MasterFileTest {
 		[Fact]
-		public async Task TestResolve() {
+		public async Task TestResolveMatchingCase() {
 			MasterFile masterFile = new MasterFile();
 			masterFile.AddIPAddressResourceRecord("google.com", "192.168.0.1");
-			{
+			await AssertGoogleResult(RecordType.A, "google.com");
+			await AssertGoogleResult(RecordType.A, "gooGLE.com");
+			await AssertGoogleResult(RecordType.ANY, "google.com");
+			await AssertGoogleResult(RecordType.ANY, "gooGLE.com");
+
+			async Task AssertGoogleResult(RecordType requestType, string question) {
 				//Test matching case.
 				IRequest clientRequest = new Request();
-				Question clientRequestQuestion = new Question(new Domain("google.com"), RecordType.A);
+				Question clientRequestQuestion = new Question(new Domain(question), requestType);
 
 				clientRequest.Id = 1;
 				clientRequest.Questions.Add(clientRequestQuestion);
@@ -33,67 +38,13 @@ namespace DNS.Tests.Server {
 
 				Question clientResponseQuestion = clientResponse.Questions[0];
 
-				Assert.Equal(RecordType.A, clientResponseQuestion.Type);
-				Assert.Equal("google.com", clientResponseQuestion.Name.ToString());
+				Assert.Equal(requestType, clientResponseQuestion.Type);
+				Assert.Equal(question, clientResponseQuestion.Name.ToString());
 
 				var clientResponseAnswer = clientResponse.AnswerRecords[0];
 				Assert.Equal(RecordType.A, clientResponseAnswer.Type);
 				Assert.Equal("google.com", clientResponseAnswer.Name.ToString());
 				Assert.Equal("192.168.0.1", ((IPAddressResourceRecord)clientResponseAnswer).IPAddress.ToString());
-			}
-			{
-				//Test different casing.
-				IRequest clientRequest = new Request();
-				Question clientRequestQuestion = new Question(new Domain("gooGLE.com"), RecordType.A);
-
-				clientRequest.Id = 1;
-				clientRequest.Questions.Add(clientRequestQuestion);
-				clientRequest.OperationCode = OperationCode.Query;
-
-				IResponse clientResponse = await masterFile.Resolve(clientRequest);
-
-				Assert.Equal(1, clientResponse.Id);
-				Assert.Equal(1, clientResponse.Questions.Count);
-				Assert.Equal(1, clientResponse.AnswerRecords.Count);
-				Assert.Equal(0, clientResponse.AuthorityRecords.Count);
-				Assert.Equal(0, clientResponse.AdditionalRecords.Count);
-
-				Question clientResponseQuestion = clientResponse.Questions[0];
-
-				Assert.Equal(RecordType.A, clientResponseQuestion.Type);
-				Assert.Equal("gooGLE.com", clientResponseQuestion.Name.ToString());
-
-				var clientResponseAnswer = clientResponse.AnswerRecords[0];
-				Assert.Equal(RecordType.A, clientResponseAnswer.Type);
-				Assert.Equal("google.com", clientResponseAnswer.Name.ToString());
-				Assert.Equal("192.168.0.1", ((IPAddressResourceRecord)clientResponseAnswer).IPAddress.ToString());
-			}
-			{
-				//Test RecordType.ANY
-				IRequest clientRequest = new Request();
-				Question clientRequestQuestion = new Question(new Domain("google.com"), RecordType.ANY);
-
-				clientRequest.Id = 1;
-				clientRequest.Questions.Add(clientRequestQuestion);
-				clientRequest.OperationCode = OperationCode.Query;
-
-				IResponse clientResponse = await masterFile.Resolve(clientRequest);
-
-				Assert.Equal(1, clientResponse.Id);
-				Assert.Equal(1, clientResponse.Questions.Count);
-				Assert.Equal(1, clientResponse.AnswerRecords.Count);
-				Assert.Equal(0, clientResponse.AuthorityRecords.Count);
-				Assert.Equal(0, clientResponse.AdditionalRecords.Count);
-
-				Question clientResponseQuestion = clientResponse.Questions[0];
-
-				Assert.Equal(RecordType.ANY, clientResponseQuestion.Type);
-				Assert.Equal("google.com", clientResponseQuestion.Name.ToString());
-
-				var clientResponseAnswer = clientResponse.AnswerRecords[0];
-				Assert.Equal(RecordType.A, clientResponseAnswer.Type);
-				Assert.Equal("google.com", clientResponseAnswer.Name.ToString());
-				Assert.Equal("192.168.0.1", ((IPAddressResourceRecord) clientResponseAnswer).IPAddress.ToString());
 			}
 		}
 	}
